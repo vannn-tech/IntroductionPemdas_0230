fccc5b9c2512f2136872fcfc54fbffcd08297417
vannn-tech <jovan20166@gmail.com>
2026-02-23 19:13:46 +0700

    langkah 1 -3

--- coba2.cpp
+++ coba2.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main(){ //mulai
     int p, l, luas;
 
+    //langkah 1
+    cout << "Masukkan panjang: ";
+    cin >> p;
 
+    //langkah 2
+    cout << "Masukkan lebar: ";
+    cin >> l;
+
+    //langkah 3
+    luas = p * l;
+    cout << "Luas persegi panjang adalah: " << luas << endl;
+
+//selesai
 }
